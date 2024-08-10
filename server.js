f5d6422f2f0b5638c43dd8271c43e2d19658241b
nikhilsinha822 <nikhilsinha822@gmail.com>
2024-08-10 18:02:13 +0530

    added the global error handler

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3500
 const cors = require('cors')
 const mongoose = require('mongoose')
 const connectDB = require('./config/dbConfig')
+const errorMiddleware = require('./middleware/error')
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,8 @@ app.use(express.urlencoded({extended: true}))
 
 connectDB();
 
+
+app.use(errorMiddleware);
 
 mongoose.connection.once('open', () => {
     console.log('connected to database')
